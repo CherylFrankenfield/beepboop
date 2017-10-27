032cfeb6e5a14d0d36d4ef2852ebdc03d55b2ad9
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2017-10-27 13:43:58 -0700

    Define function and edit loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,27 @@
-// Business logic goes here
+// // Business logic goes here
 
-var splitsValue = function()
-  for (var index = 0; index <= 10; index +=1) {
+
+var machineOuput = function(digits) {
+  var splitDigits = digits.split("");
+  var result = [];
+
+  for (var index = 0; index < splitDigits.length; index +=1) {
     if (splitDigits[index] === 0) {
-      splitDigits.replace(0, "Beep");
+      splitDigits[index].replace(0, "Beep");
+      splitDigits[index].push(result);
     } else if (splitDigits[index] === 1) {
-      splitDigits.replace(1, "Boop");
-    } else if (splitDigits[index] === 3) {
-      splitDigits.replace(3, "I'm sorry, Dave. I'm afraid I can't do that.");
-    } else if (splitDigits[index] === 9) {
-      splitDigits.replace(9, "I'm sorry, Dave. I'm afraid I can't do that.");
+      splitDigits[index].replace(1, "Boop");
+      splitDigits[index].push(result);
+    } else if (splitDigits[index] % 3 == 0) {
+      splitDigits[index].replace(splitDigits, "I'm sorry, Dave. I'm afraid I can't do that.");
+      splitDigits[index].push(result);
     } else {
       splitDigits[index] === splitDigits
+      splitDigits[index].push(result);
     }
+    return result.join("");
   }
+}
 
 //Interface goes here
 $(document).ready(function() {
@@ -21,11 +29,10 @@ $(document).ready(function() {
   event.preventDefault();
 
   var digits = parseInt($("input#digits").val());
-  var splitDigits = digits.split("");
 
-  var almostSpeak = splitsValue(splitDigits);
-  var returnSpeak = almostSpeak.join("");
-  $("#returnSpeak").show();
+  var almostSpeak = machineOutput(digits);
+  $("#returnSpeak").show(almostSpeak);
+
 
   });
 });
