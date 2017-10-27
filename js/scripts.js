4d0520dc603d96fd5ebe37fe1c0c40d11d9a8c2c
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2017-10-27 16:21:51 -0700

    User input will show correct number of loops

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,12 @@
 
 function robot(digits) {
   var splitDigits = digits.split(""); //turns input into an array
-  var result = [ ];
+  var result = [];
 
-  for (var index = 0; index < splitDigits.length; index +=1) {
+  for (var index = 0; index < splitDigits.length; index++) {
     if (splitDigits[index] === 0) {
-      splitDigits[index].replace(0, "Beep");
-      result.push("splitDigits[index]");
-      console.log(digits);
+      var newDigit1 = splitDigits[index].replace("0", "Beep");
+      result.push("newDigit1");
     } else if (splitDigits[index] === 1) {
       splitDigits[index].replace(1, "Boop");
       result.push("splitDigits[index]");
@@ -30,7 +29,7 @@ $(document).ready(function() {
   var digits = $("input#digits").val();
 
   var almostSpeak = robot(digits); //calls robot function on integer, now named digits
-  $("#returnSpeak").show(almostSpeak);
-
+  // $("#returnSpeak").show(almostSpeak); // did not work
+  $("#returnSpeak").append("<p>" + almostSpeak + "</p>") //displays something
   });
 });
