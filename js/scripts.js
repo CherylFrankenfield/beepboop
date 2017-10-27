8d394212f37be51b99df0bbb177f9c1895a720f0
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2017-10-27 12:16:14 -0700

    Broke out business and interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,31 @@
 // Business logic goes here
-var outPut = function(splitDigits)
 
+var splitsValue = function()
+  for (var index = 0; index <= 10; index +=1) {
+    if (splitDigits[index] === 0) {
+      splitDigits.replace(0, "Beep");
+    } else if (splitDigits[index] === 1) {
+      splitDigits.replace(1, "Boop");
+    } else if (splitDigits[index] === 3) {
+      splitDigits.replace(3, "I'm sorry, Dave. I'm afraid I can't do that.");
+    } else if (splitDigits[index] === 9) {
+      splitDigits.replace(9, "I'm sorry, Dave. I'm afraid I can't do that.");
+    } else {
+      splitDigits[index] === splitDigits
+    }
+  }
 
 //Interface goes here
 $(document).ready(function() {
   $("form#talkForm").submit(function(event) {
   event.preventDefault();
 
-  var result = [ ];
-  var Beeps = [ ];
-  var Boops = [ ];
-
   var digits = parseInt($("input#digits").val());
   var splitDigits = digits.split("");
 
-
-    for (var index = 0; index < splitDigits.length; index +=1) {
-      if (splitDigits === 0) {
-        Beeps.push(splitDigits);
-      } else if (digits === 1) {
-        Boops.push(splitDigits);
-      } else {
-        alert("Do not compute. Please enter number.");
-      }
-    }
-    result = Beeps + Boops;
-    return result;
-
-  // $("#returnSpeak").show(finalResponse);
+  var almostSpeak = splitsValue(splitDigits);
+  var returnSpeak = almostSpeak.join("");
+  $("#returnSpeak").show();
 
   });
 });
